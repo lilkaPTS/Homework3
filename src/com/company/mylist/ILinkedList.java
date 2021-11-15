7fc54597d0248d0ff235a2060d7f1e41ea061b0b
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2021-11-16 00:18:08 +0300

    Initial commit

---
+++ src/com/company/mylist/ILinkedList.java
@@ -0,0 +1,16 @@
+package com.company.mylist;
+
+import java.util.Iterator;
+
+public interface ILinkedList<E> extends java.lang.Iterable<E> {
+    void add(E element);
+    void add(int index, E element);
+    void clear();
+    E get(int index);
+    int indexOf(E element);
+    E remove(int index);
+    E set(int index, E element);
+    int size();
+    E[] toArray();
+    String toString();
+}
---
+++ src/com/company/mylist/MyLinkedList.java
@@ -0,0 +1,110 @@
+package com.company.mylist;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.function.Consumer;
+
+public class MyLinkedList<E> implements ILinkedList<E> {
+
+    int size = 0;
+    Node<E> first;
+    Node<E> last;
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    Node<E> node(int index) {
+        return null;
+    }
+
+    public MyLinkedList(){}
+
+    @Override
+    public void add(E element) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, element, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        return 0;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public E[] toArray() {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Itr();
+    }
+
+    private class Itr implements Iterator<E> {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public E next() {
+            return null;
+        }
+
+        @Override
+        public void remove() {
+            Iterator.super.remove();
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super E> action) {
+            Iterator.super.forEachRemaining(action);
+        }
+    }
+}
